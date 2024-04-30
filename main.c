1a26d70b7f015ca5b0c07c57ebd3649e7b85d805
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-04-30 18:17:22 +0200

    done

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int main () {
 	int num = 0;
 	printf("Encrypt:\n");
-	printf("1 - 1st lvl (Reverse + Vigenère cipher)\n2 - 2nd lvl (bit chaos)\n3 - 3rd lvl (everything)\n");
+	printf("1 - 1st lvl (Reverse + Vigenère cipher)\n2 - 2nd lvl (bit chaos)\n3 - 3rd lvl (reverse + bit chaos)\n");
 	printf("Decrypt:\n");
 	printf("4 - 1st lvl decrypt\n5 - 2nd lvl decrypt\n6 - 3rd lvl decrypt\n");
 	printf("\nEnter number: ");
@@ -18,10 +18,14 @@ int main () {
 	printf("Enter your text: ");
 	char c;
 	scanf("%c", &c);
-	if(num != 5) {
-	fgets(input, 999, stdin);
+	if(num != 5 && num != 6) {
+		fgets(input, 999, stdin);
 	} else {
-	fgets((char *)uinput, 999, stdin);
+		unsigned char tmp[999];
+		fgets((char *)tmp, 999, stdin);
+		for (int i = 1, j = 0; i < uscharlen(tmp); i+=2, j++) {
+			uinput[j] = tmp[i];
+		}
 	}
 
 	if(num == 1) {
@@ -53,7 +57,8 @@ int main () {
 		for(int i=0; i < size; i++) {
 			printf("%02x ", output[i]);
 		}
-	} else if(num == 5) { // what to input ?
+		printf("\n");
+	} else if(num == 5) {
 		unsigned char output[999];
 		bit_decrypt(uinput, output);
 		printf("%s\n", output);
@@ -61,34 +66,21 @@ int main () {
 		//reverse
 		char output[999];
 		reverse(input, output);
-		//vigenere
-		char key[999];
-		char second[999];
-		printf("Enter key for Vigenère cipher: ");
-		fgets(key, 999, stdin);
-		vigenere_encrypt(key, output, second);
 		//bits
 		unsigned char final[999];
-		bit_encrypt(input, final);
-		int size = strlen(input) - 1;
-		for(int i=0; i < size; i++) {
-			printf("%02x ", output[i]);
+		bit_encrypt(output, final);
+		int size = uscharlen(final);
+		for(int i=1; i < size; i++) {
+			printf("%02x ", final[i]);
 		}
-		printf("%s\n", final);
 	}
-	else if(num == 6) { // what to input?
+	else if(num == 6) { 
 		//bits
 		unsigned char output[999];
-		bit_encrypt(input, output);
-		//vigenere
-		char key[999];
-		char second[999];
-		printf("Enter key for Vigenère cipher: ");
-		fgets(key, 999, stdin);
-		vigenere_encrypt(key, (char *)output, second);
+		bit_decrypt(uinput, output);
 		//reverse
 		char final[999];
-		reverse(input, final);
+		reverse((char*)output, final);
 		printf("%s\n", final);
 	}
 }
